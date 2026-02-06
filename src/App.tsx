a40168a98ba76f5e719ce337a15e9d75f32b0931
Leomar Vaz Sartor <leomar.sartor@funcionalcorp.com.br>
2026-02-06 17:48:07 -0300

    start template with configuration

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export function App() {
+  return (
+    <>
+      <div className='h-screen bg-green-300'>
+        <h1> Template Inicial com Tailwind CSS</h1>
+      </div>
+    </>
+  )
+}
