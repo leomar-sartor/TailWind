f9dec12f83f306f526fad0d4f75625fd965817f5
Leomar Vaz Sartor <leomar.sartor@funcionalcorp.com.br>
2026-02-09 11:44:35 -0300

    Configuracão do Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 export function App() {
   return (
     <>
-      <div className='h-screen bg-green-300'>
+      <div className='h-screen bg-padrao'>
         <h1> Template Inicial com Tailwind CSS</h1>
       </div>
     </>
